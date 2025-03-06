10eb945053f42ecf0be96980456ae104f99edad9
mkatopola <moffatkatopola@gmail.com>
2025-03-06 10:21:15 +0200

    W01 Individual Activity: Add Item to Cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || []; // get a list(array) of cart items from local storage
+  cartItems.push(product); // add product to the list
+  setLocalStorage("so-cart", cartItems); // save the list to local storage
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
