1832e52f4602c873dea58968d390d34d3dd10d9e
Obito2912 <https://obito2912.github.io/se_project_aroundtheus/>
2025-03-13 21:22:16 -0600

    Followed instructions for Team Activity to change content on page dinamically

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -22,8 +22,11 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
+// helper to get parameter strings
 export function getParam(param) {
   const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const product = urlParams.get("product")
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+
+  return product;
 }
