aaf28caa3faad8cd606d94556a8a2356f6e43fc0
Obito2912 <https://obito2912.github.io/se_project_aroundtheus/>
2025-03-21 17:39:45 -0600

    Change name on vite, remove footer text from index.html, add loadHeaderFooter to product-listing.js

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,8 +1,10 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const productData = new ProductData("tents");
 const listOfProducts = document.querySelector(".product-list");
 const productList = new ProductList("tents", productData, listOfProducts);
 
 productList.init();
+loadHeaderFooter();
