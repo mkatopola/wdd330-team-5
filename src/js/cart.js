3bc5ddf4d36d96f682a4e9831a2b7abd03ebee15
RoikejuniorHaringanji <roikejunior07@gmail.com>
2025-03-28 15:37:00 +0300

    cart template

--- src/js/cart.js
+++ src/js/cart.js
@@ -86,24 +86,4 @@ function renderCartTotal(cartItems) {
   }
 }
 
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-  <button class="remove" data-id="${item.Id}">X</button>
-</li>`;
-
-  return newItem;
-}
-
 renderCartContents();
