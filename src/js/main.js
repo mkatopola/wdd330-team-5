ebd26656160dc30343a4c6a9114b00b260552830
Offizil <fredoffizil@gmail.com>
2025-03-29 20:10:15 +0100

    added customizable alerts

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 import { loadHeaderFooter } from "./utils.mjs";
+// here i come
+import Alert from "./alert.mjs";
+
+const alertInstance = new Alert("/json/alerts.json");
+alertInstance.render();
+//
 
 loadHeaderFooter();
