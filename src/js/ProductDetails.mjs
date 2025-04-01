778618f6e31969cd90da8db66f0d96397a006d61
Arbitermartin <arbitermartin04@gmail.com>
2025-04-01 22:22:04 +0300

    add changes

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,7 +1,8 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function productDetailsTemplate(product) {
-    return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+  return `
+  <section class="product-detail"> <h3>${product.Brand.Name}</h3>
       <h2 class="divider">${product.NameWithoutBrand}</h2>
       <img
         class="divider"
@@ -16,7 +17,8 @@ function productDetailsTemplate(product) {
       <div class="product-detail__add">
         <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
       </div></section>`;
-  }
+}
+
 export default class ProductDetails {
   constructor(productId, dataSource) {
     this.productId = productId;
@@ -32,28 +34,27 @@ export default class ProductDetails {
     // once the HTML is rendered we can add a listener to Add to Cart button
     // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
     document
-    .getElementById("addToCart")
-    .addEventListener("click", this.addToCart.bind(this));
+      .getElementById("addToCart")
+      .addEventListener("click", this.addToCart.bind(this));
   }
 
   addToCart() {
     let cartItems = getLocalStorage("so-cart");
 
-  // Ensure cartItems is an array
-  if (!Array.isArray(cartItems)) {
-    cartItems = []; // If it's not an array, set it to an empty array
-  }
+    // Ensure cartItems is an array
+    if (!Array.isArray(cartItems)) {
+      cartItems = []; // If it's not an array, set it to an empty array
+    }
 
-  cartItems.push(this.product); // Add the current product to the array
-  setLocalStorage("so-cart", cartItems);
+    cartItems.push(this.product); // Add the current product to the array
+    setLocalStorage("so-cart", cartItems);
   }
-
 
   renderProductDetails() {
     const element = document.querySelector(".product-detail");
     element.insertAdjacentHTML(
-        "afterbegin",
-        productDetailsTemplate(this.product)
+      "afterbegin",
+      productDetailsTemplate(this.product)
     );
   }
 }
