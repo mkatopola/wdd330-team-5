496bcadf7322e6078a34a6279ae7ed843c5bbd67
Arbitermartin <arbitermartin04@gmail.com>
2025-04-01 22:25:51 +0300

    add changes

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -50,8 +50,15 @@ export function setClick(selector, callback) {
 export function renderWithTemplate(template, parentElement, data, callback) {
   parentElement.innerHTML = template;
   if (callback) {
-    callback(data);
+    const number = data.length;
+    callback(number, parentElement);
   }
+}
+
+export function makeCartBadge(number, parentElement) {
+  const svgElement = parentElement.querySelector("svg");
+  const badge = `<span class="cart-badge">${number}</span>`;
+  svgElement.insertAdjacentHTML("afterend", badge);
 }
 
 export async function loadTemplate(path) {
@@ -62,9 +69,36 @@ export async function loadTemplate(path) {
 
 export async function loadHeaderFooter() {
   const headerTemplate = await loadTemplate("../partials/header.html");
+  const cartData = getLocalStorage("so-cart");
   const footerTemplate = await loadTemplate("../partials/footer.html");
   const headerElement = document.querySelector("#main-header");
   const footerElement = document.querySelector("#main-footer");
-  renderWithTemplate(headerTemplate, headerElement);
-  renderWithTemplate(footerTemplate, footerElement);
+  renderWithTemplate(headerTemplate, headerElement, cartData, makeCartBadge); renderWithTemplate(footerTemplate, footerElement);
+}
+
+export function alertMessage(message, scroll = true, duration = 3000) {
+  const alert = document.createElement("div");
+  alert.classList.add("alert");
+  alert.innerHTML = `<p>${message}</p><span>X</span>`;
+
+  alert.addEventListener("click", function (e) {
+    if (e.target.tagName == "SPAN") {
+      main.removeChild(this);
+    }
+  });
+  const main = document.querySelector("main");
+  main.prepend(alert);
+  // make sure they see the alert by scrolling to the top of the window
+  //we may not always want to do this...so default to scroll=true, but allow it to be passed in and overridden.
+  if (scroll) window.scrollTo(0, 0);
+
+  // left this here to show how you could remove the alert automatically after a certain amount of time.
+  // setTimeout(function () {
+  //   main.removeChild(alert);
+  // }, duration);
+}
+
+export function removeAllAlerts() {
+  const alerts = document.querySelectorAll(".alert");
+  alerts.forEach((alert) => document.querySelector("main").removeChild(alert));
 }
