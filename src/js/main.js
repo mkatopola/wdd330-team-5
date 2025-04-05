3d98c35ecd186575d9748af4022e023d06b3878d
Offizil <fredoffizil@gmail.com>
2025-04-05 03:16:35 +0100

    added Call to action - register with site to get discount

--- src/js/main.js
+++ src/js/main.js
@@ -28,3 +28,21 @@ document
       messageElement.style.color = "red";
     }
   });
+
+  function showWelcomeModal() {
+    const modalShown = localStorage.getItem("welcome-modal-shown");
+  
+    if (!modalShown) {
+      const modal = document.getElementById("welcome-modal");
+      modal.classList.remove("hidden");
+  
+      document.querySelector(".close-button").addEventListener("click", () => {
+        modal.classList.add("hidden");
+        localStorage.setItem("welcome-modal-shown", "true");
+      });
+    }
+  }
+  
+  // Call this when the page loads
+  document.addEventListener("DOMContentLoaded", showWelcomeModal);
+  
